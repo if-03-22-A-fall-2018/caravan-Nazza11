38c37a701b5e1991f9c31b67e2039c06a6972dbb
Vinzent Kronreif <vini.kro@gmail.com>
2019-01-08 11:40:34 +0100

    added few things

--- caravan.cpp
+++ caravan.cpp
@@ -9,35 +9,106 @@
  * Description:
  * <your description here>
  *-----------------------------------------------------------------------------
-*/
-Caravan new_caravan()
-{
-}
+ */
+ #include "caravan.h"
+ #include "general.h"
+ #include <stdlib.h>
 
-int get_length(Caravan caravan)
-{
-}
+ typedef struct _node{
+   PackAnimal pack_animal;
+   struct _node* next;
+ }Node;
 
-void delete_caravan(Caravan caravan)
-{
-}
+ struct CaravanImplementation{
+   Node* head;
+ };
 
-void add_pack_animal(Caravan caravan, PackAnimal animal)
-{
-}
+ Caravan new_caravan()
+ {
+   Caravan caravan = (Caravan)malloc(sizeof(struct CaravanImplementation));
+   caravan->head = 0;
+   return caravan;
+ }
 
-void remove_pack_animal(Caravan caravan, PackAnimal animal)
-{
-}
+ int get_length(Caravan caravan)
+ {
+   int count=0;
+   Node* currentNode=caravan->head;
 
-int get_caravan_load(Caravan caravan)
-{
-}
+   while(currentNode!=0)
+   {
+     count++;
+     currentNode=currentNode->next;
+   }
+   return count;
+ }
 
-void unload(Caravan caravan)
-{
-}
+ void delete_caravan(Caravan caravan)
+ {
+   Node* head=caravan->head;
 
-int get_caravan_speed(Caravan caravan)
-{
-}
+   while(caravan->head!=0)
+   {
+     head=caravan->head;
+     caravan->head=head->next;
+     sfree(head);
+   }
+
+   sfree(caravan);
+ }
+
+ void add_pack_animal(Caravan caravan, PackAnimal animal)
+ {
+   if((animal == 0) || (get_caravan(animal) == caravan)){
+     return;
+   }
+
+   if(get_caravan(animal) != 0){
+     remove_pack_animal(get_caravan(animal), animal);
+   }
+
+   add_to_caravan(animal, caravan);
+   Node* node= (Node*)malloc(sizeof(Node));
+   node->pack_animal = animal;
+   node->next = caravan->head;
+   caravan->head = node;
+
+ }
+
+ void remove_pack_animal(Caravan caravan, PackAnimal animal)
+ {
+      
+ }
+
+ int get_caravan_load(Caravan caravan)
+ {
+   int length = get_length(caravan);
+   int load_count = 0;
+
+   Node* currentNode = caravan->head;
+
+   for(int i = 0; i < length; i++){
+     load_count += get_load(currentNode->pack_animal);
+     currentNode = currentNode->next;
+   }
+
+   return load_count;
+ }
+
+ void unload(Caravan caravan)
+ {
+   int length = get_length(caravan);
+
+   Node* currentNode = caravan->head;
+
+   for(int i = 0; i < length; i++){
+     unload(currentNode->pack_animal);
+     currentNode = currentNode->next;
+   }
+
+ }
+
+ int get_caravan_speed(Caravan caravan)
+ {
+   return 0;
+ }
--- caravan.h
+++ caravan.h
@@ -10,59 +10,59 @@
  * <Add description>
  * ----------------------------------------------------------
  */
-#ifndef CARAVAN_H
-#define CARAVAN_H
+ #ifndef CARAVAN_H
+ #define CARAVAN_H
 
-#include "pack_animal.h"
+ #include "pack_animal.h"
 
-/**
-* Initialisiert eine Karawane. D. h., dass es alle in der Karawane befindlichen Tiere aus der
-* Karawane entfernt und die Karawane als leer kennzeichnet.
-* @param caravan Die Karawane, welche initialisiert werden soll.
-*/
-Caravan new_caravan();
+ /**
+ * Initialisiert eine Karawane. D. h., dass es alle in der Karawane befindlichen Tiere aus der
+ * Karawane entfernt und die Karawane als leer kennzeichnet.
+ * @param caravan Die Karawane, welche initialisiert werden soll.
+ */
+ Caravan new_caravan();
 
-/**
-* @return Die Länge einer Karawane, d.h., die Anzahl der Tiere in der Karawane.
-*/
-int get_length(Caravan caravan);
+ /**
+ * @return Die Länge einer Karawane, d.h., die Anzahl der Tiere in der Karawane.
+ */
+ int get_length(Caravan caravan);
 
-/**
-* Entfern alle Tiere aus einer Karawane und löscht die Karawane.
-*/
-void delete_caravan(Caravan caravan);
+ /**
+ * Entfern alle Tiere aus einer Karawane und löscht die Karawane.
+ */
+ void delete_caravan(Caravan caravan);
 
-/**
-* Fügt ein Packtier zu einer Karawane hinzu.
-*/
-void add_pack_animal(Caravan caravan, PackAnimal animal);
+ /**
+ * Fügt ein Packtier zu einer Karawane hinzu.
+ */
+ void add_pack_animal(Caravan caravan, PackAnimal animal);
 
-/**
-* Entfernt ein Packtier aus einer Karawane.
-*/
-void remove_pack_animal(Caravan caravan, PackAnimal animal);
+ /**
+ * Entfernt ein Packtier aus einer Karawane.
+ */
+ void remove_pack_animal(Caravan caravan, PackAnimal animal);
 
-/**
-* @return Die Geschwindigkeit einer Karawane. Diese richtet sich nach der Geschwindigkeit des
-* langsamsten Tiers in der Karawane.
-*/
-int get_caravan_speed(Caravan caravan);
+ /**
+ * @return Die Geschwindigkeit einer Karawane. Diese richtet sich nach der Geschwindigkeit des
+ * langsamsten Tiers in der Karawane.
+ */
+ int get_caravan_speed(Caravan caravan);
 
-/**
-* @return Die Gesamtanzahl der Ballen einer Karawane.
-*/
-int get_caravan_load(Caravan caravan);
+ /**
+ * @return Die Gesamtanzahl der Ballen einer Karawane.
+ */
+ int get_caravan_load(Caravan caravan);
 
-/**
-* Entl&auml;dt die gesamte Karawane. D.h. es wird jedes Tier in der Karawane von seinen Ballen
-* befreit.
-*/
-void unload(Caravan caravan);
+ /**
+ * Entl&auml;dt die gesamte Karawane. D.h. es wird jedes Tier in der Karawane von seinen Ballen
+ * befreit.
+ */
+ void unload(Caravan caravan);
 
-/**
-* Verteilt die Last der Ballen so auf die einzelnen Tiere, dass die Geschwindigkeit der Karawane
-* maximal ist.
-*/
-void optimize_load(Caravan caravan);
+ /**
+ * Verteilt die Last der Ballen so auf die einzelnen Tiere, dass die Geschwindigkeit der Karawane
+ * maximal ist.
+ */
+ void optimize_load(Caravan caravan);
 
-#endif
+ #endif
